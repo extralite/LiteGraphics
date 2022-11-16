396f255c8c54f120e5c32e16f10f25f4ea0fc849
Vitor Mac <5874907+listanegra@users.noreply.github.com>
2022-11-15 23:02:16 -0300

    Namespaced functions

--- include/xlite/LiteGraphics.hpp
+++ include/xlite/LiteGraphics.hpp
@@ -1,8 +1,20 @@
 #pragma once
 
-class LiteGraphics {
-public:
-    static int Load();
+#include <cstdint>
 
-private:
-};
+namespace Lite {
+
+    namespace Graphics {
+        int Load();
+
+        void Clear();
+        void ClearColor(float r, float g, float b, float a);
+
+        void EnableDepthTesting();
+        void EnableBlending();
+        void EnableTexture2D();
+
+        void ViewPort(int32_t x, int32_t y, int32_t width, int32_t height);
+    }// namespace Graphics
+
+}// namespace Lite
--- src/LiteGraphics.cpp
+++ src/LiteGraphics.cpp
@@ -1,9 +1,10 @@
 #include <xlite/LiteGraphics.hpp>
+
 #include <glad/gl.h>
 
 #include <iostream>
 
-int LiteGraphics::Load() {
+int Lite::Graphics::Load() {
     int gl_version = gladLoaderLoadGL();
 
     if (!gl_version) {
@@ -12,7 +13,34 @@ int LiteGraphics::Load() {
     }
 
     const char *version = reinterpret_cast<const char *>(glGetString(GL_VERSION));
-    std::cout << "Initialized LiteGraphics " << version << std::endl;
+    std::cout << "Initialized LiteGraphics OpenGL " << version << std::endl;
 
     return 1;
+}
+
+void Lite::Graphics::Clear() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void Lite::Graphics::ClearColor(float r, float g, float b, float a) {
+    glClearColor(r, g, b, a);
+}
+
+void Lite::Graphics::EnableDepthTesting() {
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glClearDepth(1.f);
+}
+
+void Lite::Graphics::EnableBlending() {
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+void Lite::Graphics::EnableTexture2D() {
+    glEnable(GL_TEXTURE_2D);
+}
+
+void Lite::Graphics::ViewPort(int32_t x, int32_t y, int32_t width, int32_t height) {
+    glViewport(x, y, width, height);
 }
